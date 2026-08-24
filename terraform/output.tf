25a451bbd5355b8b62a964a7f7dccb994362fbcb
Femi Fakolujo <floboss007@gmail.com>
2026-08-24 22:54:22 +0100

    terraform added

---
+++ terraform/output.tf
@@ -0,0 +1,19 @@
+output "vm_external_ip" {
+  description = "External IP of the CI stack VM"
+  value       = google_compute_instance.todo_project_vm.network_interface[0].access_config[0].nat_ip
+}
+
+output "jenkins_url" {
+  value = "http://${google_compute_instance.todo_project_vm.network_interface[0].access_config[0].nat_ip}:8080"
+}
+
+output "web_url" {
+  value = "http://${google_compute_instance.todo_project_vm.network_interface[0].access_config[0].nat_ip}:8081"
+}
+
+
+
+output "github_webhook_url" {
+  description = "Paste this into GitHub repo Settings -> Webhooks -> Payload URL"
+  value       = "http://${google_compute_instance.todo_project_vm.network_interface[0].access_config[0].nat_ip}:8080/github-webhook/"
+}
---
+++ terraform/startup-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Install Docker
+apt-get update
+apt-get install -y ca-certificates curl gnupg git
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Clone the repo and bring the stack up
+cd /opt
+git clone ${repo_url} todo-project
+cd todo-project
+docker compose up -d --build
